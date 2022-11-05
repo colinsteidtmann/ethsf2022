6f9713bccd15f43a38f44f5a0bfa41de559a4d5e
Colin Steidtmann <colinsteidtmann@berkeley.edu>
2022-11-05 16:34:29 -0700

    dd

--- frontend/pages/auction/[tokenID].js
+++ frontend/pages/auction/[tokenID].js
@@ -1,15 +1,21 @@
-import { useContractReads } from 'wagmi';
+import { useContract, useProvider, useSigner, useContractReads } from 'wagmi';
 import { AUCTION_ABI, AUCTION_ADDRESS } from "../../lib/contract.js";
 import BidForm from '../../components/CreateBid.js';
 import { useState } from 'react';
 
 export default function Auction() {
     const [started, setStarted] = useState(false);
+    const provider = useProvider();
+    const { data: signer } = useSigner();
     // Contract 
     const auctionContract = {
         address: AUCTION_ADDRESS,
         abi: AUCTION_ABI,
+        signerOrProvider: signer || provider
     };
+    const contract = useContract({
+        ...auctionContract
+    });
     const readResult = useContractReads({
         contracts: [
             {
@@ -26,10 +32,15 @@ export default function Auction() {
         console.log(data);
     };
 
-    return (
-        <>
-            <p>Hello</p>
-        </>
-    );
+    if (contract) {
+        return (
+            <>
+                <BidForm contract={contract} />
+            </>
+        );
+    } else {
+        return (<div>Loading ...</div>);
+    }
+
 
 }
