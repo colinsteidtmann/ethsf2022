b0dd05e6b15b2fe447d6edbb04c2b046156d67e5
Colin Steidtmann <colinsteidtmann@berkeley.edu>
2022-11-05 15:52:09 -0700

    alchemy api

--- frontend/lib/contract.js
+++ frontend/lib/contract.js
@@ -1,5 +1,5 @@
 import NFTABI from "./NFT.json";
-import AUCTIONABI from "./NFT.json";
+import AUCTIONABI from "./Auction.json";
 export const NFT_ADDRESS = "0xE2769403A4CbE86894810a5a4385F1E2e7491dA3";
 export const AUCTION_ADDRESS = "0xd187365573e1872ed93C70c719DbCbcb56c8Ac4A";
 export const NFT_ABI = NFTABI.abi;
