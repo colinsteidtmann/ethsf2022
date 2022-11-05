522f65a34d779755430fbd4c47b59961bfd04f0c
Colin Steidtmann <colinsteidtmann@berkeley.edu>
2022-11-05 15:42:35 -0700

    auction

--- frontend/lib/contract.js
+++ frontend/lib/contract.js
@@ -1,3 +1,6 @@
-import ABI from "./NFT.json";
+import NFTABI from "./NFT.json";
+import AUCTIONABI from "./NFT.json";
 export const NFT_ADDRESS = "0xE2769403A4CbE86894810a5a4385F1E2e7491dA3";
-export const NFT_ABI = ABI.abi;
+export const AUCTION_ADDRESS = "0xd187365573e1872ed93C70c719DbCbcb56c8Ac4A";
+export const NFT_ABI = NFTABI.abi;
+export const AUCTION_ABI = AUCTIONABI.abi;
